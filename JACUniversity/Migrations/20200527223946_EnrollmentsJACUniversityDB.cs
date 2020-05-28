6f88fc8beca6898faddb89d1fa62510279abf8f9
oldGunCoder <57119330+oldGunCoder@users.noreply.github.com>
2020-05-27 23:01:07 -0400

    database mapped

---
+++ JACUniversity/Migrations/20200527223946_EnrollmentsJACUniversityDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JACUniversity.Migrations
+{
+    public partial class EnrollmentsJACUniversityDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
